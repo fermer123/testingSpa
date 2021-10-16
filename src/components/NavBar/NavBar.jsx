20d44fcece196b760d9a57531a4ffe3050f8ae6b
fermer123 <fermer_525@mail.ru>
2021-10-17 01:49:05 +0500

    instal

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,10 +4,10 @@ const NavBar = () => {
   return (
     <nav className={classes.nav}>
       <div className={classes.item}>
-        <a>Profile</a>
+        <a href='/profile'>Profile</a>
       </div>
       <div className={`${classes.item} ${classes.active}`}>
-        <a>Messeges</a>
+        <a href='/Dialogs'>Messeges</a>
       </div>
       <div className={classes.item}>
         <a>News</a>
