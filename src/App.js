7d658fbe73ecf60d71b643850c69769285613517
fermer123 <fermer_525@mail.ru>
2021-10-17 02:45:02 +0500

    new version npm, add route in json, new chapter

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <NavBar />
-      <div className='app-wrapper-content'>
-        <Dialogs />
-        {/* <Profile /> */}
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <NavBar />
+        <div className='app-wrapper-content'>
+          <Route path='/dialog' component={Dialogs} />
+          <Route path='/profile' component={Profile} />
+        </div>
+        <footer></footer>
       </div>
-      <footer></footer>
-    </div>
+    </BrowserRouter>
   );
 }
 
