3fc400b930fbc7a05aa404ae487b5d34b7c3b9c1
fermer123 <fermer_525@mail.ru>
2021-10-16 16:58:36 +0500

    lvl 15 change post

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,13 @@
+import classes from './Post.module.css';
+const Post = () => {
+  return (
+    <div className={classes.item}>
+      <img src='https://sun9-81.userapi.com/s/v1/ig2/D3r0uxLYycMoT6LIxDkxocJXIBq6ruCHidXAKxbDT36YdCDsHcXVmvj_H5C3kNUVRar31z2ExBJhIQ2kFixET8gl.jpg?size=200x200&quality=96&crop=10,0,950,950&ava=1'></img>
+      post 1
+      <div>
+        <span>like</span>
+      </div>
+    </div>
+  );
+};
+export default Post;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,11 @@
+import MyPosts from '../Profile/MyPosts/MyPosts';
 import classes from './Profile.module.css';
 const Profile = () => {
   return (
     <div className={classes.content}>
       <img src='http://fotorelax.ru/wp-content/uploads/2016/03/Beautiful-photos-and-pictures-on-various-subjects-01.jpg'></img>
       <div> ava + descripton</div>
-      <div>
-        my post
-        <div>new post</div>
-      </div>
-      <div className={classes.posts}>
-        <div className={classes.item}>post 1</div>
-        <div className={classes.item}>post 2</div>
-      </div>
+      <MyPosts />
     </div>
   );
 };
