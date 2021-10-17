cc2fdcf41615446e5f42955db29d15d98251c1b3
fermer123 <fermer_525@mail.ru>
2021-10-17 23:43:05 +0500

    NavLink отличие от линка только в возможности добавить класс стиля

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,33 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css';
 const Dialogs = (props) => {
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <div className={classes.dialog + ' ' + classes.active}>Igor</div>
-        <div className={classes.dialog}>any User</div>
-        <div className={classes.dialog}>any User1</div>
-        <div className={classes.dialog}>any User2</div>
-        <div className={classes.dialog}>any User3</div>
-        <div className={classes.dialog}>any User4</div>
-        <div className={classes.dialog}>any User5</div>
-        <div className={classes.dialog}>any User6</div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          <NavLink to='/Dialogs/1'>Igor </NavLink>
+        </div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          <NavLink to='/Dialogs/2'>any User </NavLink>
+        </div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          <NavLink to='/Dialogs/3'>any Userany User1 </NavLink>
+        </div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          <NavLink to='/Dialogs/4'>any Userany User2 </NavLink>
+        </div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          <NavLink to='/Dialogs/5'>any Userany User3 </NavLink>
+        </div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          <NavLink to='/Dialogs/6'>any Userany User4 </NavLink>
+        </div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          <NavLink to='/Dialogs/7'>any Userany User5 </NavLink>
+        </div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          <NavLink to='/Dialogs/8'>any Userany User </NavLink>
+        </div>
       </div>
       <div className={classes.messages}>
         <div className={classes.message}>Hi</div>
