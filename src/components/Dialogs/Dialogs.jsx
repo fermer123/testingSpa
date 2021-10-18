c0c3e0440978b359a7406b8cbd67c8d6b69d5f0c
fermer123 <fermer_525@mail.ru>
2021-10-18 20:07:14 +0500

    optimize with props dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,39 +1,36 @@
 import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css';
+
+const DialogItem = (props) => {
+  let path = '/Dialogs/' + props.id;
+  return (
+    <div className={classes.dialog + ' ' + classes.active}>
+      <NavLink to={path}>{props.name} </NavLink>
+    </div>
+  );
+};
+
+const Messages = (props) => {
+  return <div className={classes.dialog}>{props.text}</div>;
+};
 const Dialogs = (props) => {
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <div className={classes.dialog + ' ' + classes.active}>
-          <NavLink to='/Dialogs/1'>Igor </NavLink>
-        </div>
-        <div className={classes.dialog + ' ' + classes.active}>
-          <NavLink to='/Dialogs/2'>any User </NavLink>
-        </div>
-        <div className={classes.dialog + ' ' + classes.active}>
-          <NavLink to='/Dialogs/3'>any Userany User1 </NavLink>
-        </div>
-        <div className={classes.dialog + ' ' + classes.active}>
-          <NavLink to='/Dialogs/4'>any Userany User2 </NavLink>
-        </div>
-        <div className={classes.dialog + ' ' + classes.active}>
-          <NavLink to='/Dialogs/5'>any Userany User3 </NavLink>
-        </div>
-        <div className={classes.dialog + ' ' + classes.active}>
-          <NavLink to='/Dialogs/6'>any Userany User4 </NavLink>
-        </div>
-        <div className={classes.dialog + ' ' + classes.active}>
-          <NavLink to='/Dialogs/7'>any Userany User5 </NavLink>
-        </div>
-        <div className={classes.dialog + ' ' + classes.active}>
-          <NavLink to='/Dialogs/8'>any Userany User </NavLink>
-        </div>
+        <DialogItem name='Igor' id='1' />
+        <DialogItem name='any user1' id='2' />
+        <DialogItem name='any user2' id='3' />
+        <DialogItem name='any user3' id='3' />
+        <DialogItem name='any user4' id='4' />
+        <DialogItem name='any user5' id='5' />
       </div>
       <div className={classes.messages}>
-        <div className={classes.message}>Hi</div>
-        <div className={classes.message}>How are u </div>
-        <div className={classes.message}>Hi hi</div>
-        <div className={classes.message}>Hi hi hi</div>
+        <Messages text='hellow Igor' />
+        <Messages text='any user1' />
+        <Messages text='any user2' />
+        <Messages text='any user3' />
+        <Messages text='any user4' />
+        <Messages text='any user5' />
       </div>
     </div>
   );
