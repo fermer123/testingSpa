4c2626e4b513e04eba7a03781092e5f98af69b8e
fermer123 <fermer_525@mail.ru>
2021-10-24 21:16:58 +0500

    addevent

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,26 @@
+import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 const MyPosts = (props) => {
   let PostsDataElements = props.appState.map((el) => (
     <Post message={el.post} counts={el.like} />
   ));
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
   return (
     <div className={classes.postsBlock}>
       <h3>my post </h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
         <button>Remove</button>
       </div>
