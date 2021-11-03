1b003be870bb6df0f9d6a7e12bd988be296be8a6
fermer123 <fermer_525@mail.ru>
2021-11-03 17:08:26 +0500

    nothing

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
   _state: {
     profilePage: {
@@ -42,7 +45,7 @@ let store = {
   },
 
   dispatch(action) {
-    if (action.type === 'ADD-POST') {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 6,
         post: this._state.profilePage.newPostText,
@@ -51,21 +54,20 @@ let store = {
       this._state.profilePage.PostsData.push(newPost);
       this._state.profilePage.newPostText = '';
       this._callSubscriber(this._state);
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.profilePage.newPostText = action.newText;
       this._callSubscriber(this._state);
     }
   },
 };
 
-export const AddPostActionCreator = () => {
-  return {
-    type: 'ADD-POST',
-  };
-};
+export const AddPostActionCreator = () => ({
+  type: ADD_POST,
+});
 
-export const UpdateNewPostTextActionCreator = (text) => {
-  return { type: 'UPDATE-NEW-POST-TEXT', newText: text };
-};
+export const UpdateNewPostTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
 
 export default store;
